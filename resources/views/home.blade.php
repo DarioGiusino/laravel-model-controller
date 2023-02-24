c4a5d3b5e5147a6846ab82bc338762c1c0b6fee1
DarioGiusino <dario.giusino@gmail.com>
2023-02-24 17:42:55 +0100

    generated cards from data retrieved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,15 +12,18 @@
 
 <body>
   <div class="container my-4">
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
+    <div class="row">
+      @foreach ($movies as $movie)
+        <div class="card m-3" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">{{ $movie->title }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ $movie->original_title }}</h6>
+            <p class="card-text">{{ $movie->nationality }}</p>
+            <p class="card-text">{{ $movie->date }}</p>
+            <p class="card-text">{{ $movie->vote }}</p>
+          </div>
+        </div>
+      @endforeach
     </div>
   </div>
 </body>
