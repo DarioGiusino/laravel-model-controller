3e7832cfb035d98cdfc4c843a370dde83b065ff3
DarioGiusino <dario.giusino@gmail.com>
2023-02-24 17:39:04 +0100

    retrieved data from db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function home()
     {
-        return view('home');
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
     }
 }
